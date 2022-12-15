ccb145b8bac3c518450d77b00a2c7284977ed5e4
shoibaldas <shoibalshantu@gmail.com>
2022-12-16 05:05:52 +0600

    Backend server for atgworld API is designed in MVC and some utils has been declared

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const { failure } = require("./utils/commonResponse");
+const HTTP_STATUS = require("./utils/httpStatus");
+
+const app = express();
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+    res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .send(failure(`Can't ${req.method} ${req.url}`));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .send(failure("Internal Server Error!", err.message));
+});
+
+databaseConnection(() => {
+    app.listen(port, () => {
+        console.log(`ATG World Portal is running on PORT: ${port}`);
+    });
+});
---
+++ utils/responseMessage.js
@@ -0,0 +1,11 @@
+const responseMessage = {
+    loginSuccessMessage: "You have logged in successfully!",
+    signupSuccessMessage: "You have signed up in successfully!",
+    addPostSuccessMessage: "You have been posted successfully!",
+    addLikeSuccessMessage: "You have liked the post successfully!",
+    addCommentSuccessMessage: "Your comment has been posted successfully!",
+    unauthorizedLoginMessage: "Unauthorized user login!",
+
+};
+
+module.exports = responseMessage;
